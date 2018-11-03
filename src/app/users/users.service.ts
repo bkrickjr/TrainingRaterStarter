f8fd010bae19c7a8cf485d919c994cbfbe5f8905
admin <bkrickjr@gmail.com>
2018-11-03 14:35:08 -0400

    Added users-list component to routing,  created users service

---
+++ src/app/users/users.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, Inject } from '@angular/core';
+
+@Injectable()
+export class UserService {
+    usersMock = [ { Name: 'Brian Krick', SessionsCompleted: '0'},
+    { Name: 'Future Brian Krick', SessionsCompleted: '1'},
+    { Name: 'Even Further Future Brian Krick', SessionsCompleted: '3'}
+    ];
+    constructor() { }
+
+    getUsers(): {}[] {
+        return this.usersMock;
+    }
+}
