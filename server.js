bb52867aaafbeefb2f8c7f83668c1d89d3aa5def
Krick <bkrick271@cable.comcast.com>
2018-11-05 18:14:53 -0500

    created server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+//http comes from node
+var http = require('http')
+
+/*
+//Could be done this way if function call backs are not your thing, 
+//but samples from now on will have inline functions
+
+var serverConfiguration = function(req, res) {
+    res.writeHead(200);
+    res.end("Hello World");
+}
+
+var server = serverConfiguration
+*/
+
+var server = http.createServer(function(req, res){
+    res.writeHead(200);
+    res.end("Hello World");
+})
+
+server.listen(8080);
