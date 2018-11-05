f6708f3ea2b4bbf9db7dd3a7106a8b4de130a4fa
Krick <bkrick271@cable.comcast.com>
2018-11-05 18:53:47 -0500

    added post, put, delete requests

--- server_express.js
+++ server_express.js
@@ -3,7 +3,13 @@ var express = require('express')
 var port = 8080;
 var app = express();
 
-app.get( '/', (req, res) => { res.send("Hello World!_!") } )
-app.get( '/test', (req, res) => { res.send("Hello World Test Url!") }  )
+app.get( '/', (req, res) => { res.send("Hello World!_!") } );
+app.get( '/test', (req, res) => { res.send("Hello World Test Url!") }  );
 
-app.listen(port)
+app.post( '/', (req, res) => { res.send('Got Post Request') } );
+
+app.put( '/user', (req, res) => { res.send('Got Put Request') } );
+
+app.delete( '/user', (req, res) => { res.send('Got Delete Request') } );
+
+app.listen(port);
