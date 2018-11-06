e515c5888724f12f05a55670743200afd857f447
Krick <bkrick271@cable.comcast.com>
2018-11-05 19:49:46 -0500

    brought in express, created config file

---
+++ config/config.js
@@ -0,0 +1,6 @@
+CONFIG = {};
+
+CONFIG.db_dialect = 'mysql';
+CONFIG.db_name = 'TrainingRaterStarter';
+CONFIG.db_user = 'root'; //never ever use root irl!
+CONFIG.db_password = 'password'; //password used to get into db connection
