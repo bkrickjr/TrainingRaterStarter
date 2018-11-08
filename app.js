539f6678a612c01b36536290176a458df9e2aa58
Krick <bkrick271@cable.comcast.com>
2018-11-08 18:12:39 -0500

    added dev in config

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
+require('./config/config');
+const models = require('./models');
 
 const app = express();
 
 app.get( '/', (req, res) => { res.send("Helloworld!") } );
+
+models.sequelize        //accessing models\index.js which is exporting sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+if (CONFIG.app == 'dev') {
+    models.sequelize.sync();
+}
 
 module.exports = app;
--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 CONFIG = {};
 
+CONFIG.app = 'dev';
 CONFIG.db_dialect = 'mysql';
 CONFIG.db_name = 'TrainingRaterStarter';
 CONFIG.db_user = 'root'; //never ever use root irl!
