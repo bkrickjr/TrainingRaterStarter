a81f34bd6efb35cf37370cdfbee904cbbcf741b0
Krick <bkrick271@cable.comcast.com>
2018-11-08 18:19:03 -0500

    fixed DB name

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@ CONFIG = {};
 
 CONFIG.app = 'dev';
 CONFIG.db_dialect = 'mysql';
-CONFIG.db_name = 'TrainingRaterStarter';
+CONFIG.db_name = 'Bootcamp';
 CONFIG.db_user = 'root'; //never ever use root irl!
 CONFIG.db_password = ''; //password used to get into db connection
