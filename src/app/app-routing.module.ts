c0af00cfc35fa40ea0fff0dca56e1ba1641701b2
admin <bkrickjr@gmail.com>
2018-11-24 11:55:01 -0500

    Planning out the process and create the session-detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { SessionsListComponent } from './sessions/sessions-list/sessions-list.component';
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { Routes, RouterModule } from '@angular/router';
 import { UsersListComponent } from './users/users-list/users-list.component';
+import { SessionsListComponent } from './sessions/sessions-list/sessions-list.component';
+import { SessionsDetailComponent } from './sessions/session-detail/session-detail.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'sessions', component: SessionsListComponent },
+  { path: 'sessions/detail', component: SessionsDetailComponent },
   { path: 'users', component: UsersListComponent}
 ];
 
--- src/app/sessions/sessions.module.ts
+++ src/app/sessions/sessions.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 
-import { SessionsListComponent } from './sessions-list/sessions-list.component';
-import { SessionsService } from './sessions.service';
 import { CommonModule } from '@angular/common';
+import { SessionsService } from './sessions.service';
+import { SessionsListComponent } from './sessions-list/sessions-list.component';
+import { SessionsDetailComponent } from './session-detail/session-detail.component';
 
 @NgModule({
   declarations: [
       SessionsListComponent,
+      SessionsDetailComponent,
   ],
   imports: [
       CommonModule
