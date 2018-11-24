a97a318d010f81cf2437ef08574683b1c69d8ec1
admin <bkrickjr@gmail.com>
2018-11-24 11:53:23 -0500

    CORS Changes

--- app.js
+++ app.js
@@ -12,8 +12,16 @@ app.use(bodyParser.urlencoded({ entended: false }));
 
 // CORS
 app.use(function (req, res, next) {
-  // Webite you wish to allow to connect
+  // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 
+    'X-Requested-With, content-type, Authorization, Content-Type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
   // Pass to next layer of middleware
   next();
 });
